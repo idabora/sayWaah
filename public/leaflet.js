492d8858c6e0ee2e0740f820dd703a294b1af3f0
idabora <anurodhk82@gmail.com>
2024-10-05 02:34:29 +0530

    leaflet into

---
+++ public/leaflet.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([28.388094, 79.4141105], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
