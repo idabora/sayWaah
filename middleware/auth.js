ce863ec8faf8f43b5d963fb8fbf917032ff2df04
idabora <anurodhk82@gmail.com>
2024-10-05 18:40:32 +0530

    change in req body and leaflet things(ignore)

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,14 @@
+const { User } = require("../Model");
+
 module.exports.isAuth = async (req, res, next) => {
     if (req.session.isAuthenticated) {
-        req.body.userId = req.session.userId;
+
+        let user=await User.findById(req.session.userId,"firstName lastName email city isProfileCompleted")
+        req.body={...req.body,...user,...req.session.userId};
+        for(let key in user){
+            req.body[key]=user[key];
+        }
+        // req.body.userId = req.session.userId;
 
         next();
     } else {
--- public/leaflet.js
+++ public/leaflet.js
@@ -1,6 +1,34 @@
-var map = L.map('map').setView([28.388094, 79.4141105], 13);
+function mapActive() {
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    navigator.geolocation.getCurrentPosition((pos) => {
+        let { latitude, longitude } = pos.coords;
 
+        let map = L.map('map').setView([latitude, longitude], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+
+        // L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        //     attribution: '© OpenStreetMap © CartoDB',
+        //     maxZoom: 19
+        // }).addTo(map);
+
+        map.on('click', (event) => {
+            const { lat, lng } = event.latlng;
+
+            L.marker([lat,lng])
+            .addTo(map)
+            .bindPopup()
+            .setPopupContent("<p>hellooooo</p>")
+            .openPopup();
+
+        })
+
+
+
+    })
+
+}
+mapActive();
