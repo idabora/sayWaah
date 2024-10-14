9a7c47bd9dcd3b26cc945cec8df709658ce93489
idabora <anurodhk82@gmail.com>
2024-10-14 17:46:26 +0530

    new contrls

--- controllers/helpController.js
+++ controllers/helpController.js
@@ -1,4 +1,5 @@
-const { User ,Help} = require("../Model/index");
+const { User, Help } = require("../Model/index");
+const{objectId}=require('mongoose')
 
 module.exports.addForHelp = async (req, res) => {
     console.log("HELOOOOOOOOOOOOO");
@@ -7,7 +8,9 @@ module.exports.addForHelp = async (req, res) => {
     }
     return res.status(200).render('helpinfo')
 
+
 }
+
 
 module.exports.postHelp = async (req, res) => {
     try {
@@ -30,7 +33,8 @@ module.exports.postHelp = async (req, res) => {
             lastName,
             description,
             category,
-            location
+            location,
+            userId: req.user._id
         });
 
         console.log(helpDetails);
@@ -43,3 +47,29 @@ module.exports.postHelp = async (req, res) => {
         });
     }
 };
+
+module.exports.getHelpInfo = async(req,res)=>{
+try {
+    
+    const {id}=req.params;
+    console.log("++>>",req.params,id);
+    if(!id){
+        return res.status(400).json({message:"_id not found",redirectUrl:'/dashboard'})
+}
+
+let helpData = await Help.findById(id);
+console.log(helpData);
+if(!helpData){
+    return res.status(400).json({message:"Help Data not found"});
+}
+
+return res.status(200).render('helpData',{helpData});
+} catch (err) {
+    console.error(err);
+    return res.status(400).json({
+        message: "Error getting Help Info.",
+        error: err.message
+    });
+}
+
+}
--- routes/helpRoutes.js
+++ routes/helpRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {isAuth}=require('../middleware/auth')
 const {User,Help}=require('../Model/index')
 const {
-    addForHelp, postHelp
+    addForHelp, postHelp,getHelpInfo
 } = require('../controllers/helpController')
 
 router.get('/askhelp', isAuth,addForHelp);
 
 router.post('/posthelp',isAuth, postHelp);
+
+router.get('/gethelpinfo/:id',isAuth,getHelpInfo);
 
 module.exports = router;
