20d8dc5a7a534030eec29e336a7f1dae8cfdd591
idabora <anurodhk82@gmail.com>
2024-11-05 22:37:30 +0530

    minor changes

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {isAuth}=require('../middleware/auth')
-const {userChat,chatBox}=require('../controllers/chatController')
+const {userChat,chatBox,getChats}=require('../controllers/chatController')
 
 router.get('/user',isAuth,userChat)
 router.get('/chatbox',isAuth,chatBox)
+router.get('/chatlist',isAuth,getChats)
 
 
 module.exports=router;
